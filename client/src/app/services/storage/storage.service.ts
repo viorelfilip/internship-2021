77a9e73826f3f8a0473027ad7c74633367ad65f1
Viorel Filip <viorel.filip@indecosoft.ro>
2021-07-20 13:07:08 +0300

    register page

---
+++ client/src/app/services/storage/storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+
+  constructor(private storage: Storage) {
+    this.init();
+  }
+  async init() {
+    await this.storage.create();
+  }
+  async get(key: string) {
+    return await this.storage.get(key);
+  }
+  async set(key: string, data: any) {
+    await this.storage.set(key, data);
+  }
+}
+
